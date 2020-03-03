c5f76dca248dee7fd69d95fc9b5af0f76c32578e
Igor Velikorossov <igveliko@microsoft.com>
2020-03-02 20:10:44 -0800

    Initial commit
    .NET Core 3.0 app with roll forward

---
+++ Form1.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace TestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
